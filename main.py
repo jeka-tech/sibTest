cb8e8a3eadf58a07254218ab466b7184dceb94b7
JT <jeka-tech@mail.ru>
2023-02-15 14:36:49 +0400

    Added a graph by points

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from first.nums import plus
+from matplotlib.pyplot import *
+
 
 if __name__ == '__main__':
   print(plus(3,3))
+  print(3+3)
+
+  x = [3, 4, 6, 12.5, 13.4, 8.9, 7.1]
+  plot(x)
+  show()
