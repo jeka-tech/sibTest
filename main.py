c30d79a4df890866cd0d84fff4f318bfba4e39fb
JT <jeka-tech@mail.ru>
2023-02-16 16:38:50 +0400

    Testing 1

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import first
 from first.nums import plus
-from matplotlib.pyplot import *
+#from matplotlib.pyplot import *
 
 
 if __name__ == '__main__':
   print(plus(3,3))
   print(3+3)
+  print(var_init)
 
-  x = [3, 4, 6, 12.5, 13.4, 8.9, 7.1]
+'''  x = [3, 4, 6, 12.5, 13.4, 8.9, 7.1]
   plot(x)
-  show()
+  show()'''
